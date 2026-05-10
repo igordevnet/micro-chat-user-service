51872acbe28b726de64e401d939e137c164ca1a5
igordevnet <igorsouzaalmeida2404@gmail.com>
2026-05-10 14:44:50 -0300

    Chore: Removing CORS from services, it's centralizad in the gateway now

--- src/main/java/com/microservice/microchatuserservice/infrastructure/config/SecurityConfiguration.java
+++ src/main/java/com/microservice/microchatuserservice/infrastructure/config/SecurityConfiguration.java
@@ -27,16 +27,6 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> {
-                    CorsConfiguration corsConfiguration = new CorsConfiguration();
-                    corsConfiguration.addAllowedOrigin("*");
-                    corsConfiguration.addAllowedHeader("*");
-                    corsConfiguration.addAllowedMethod("*");
-
-                    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-                    source.registerCorsConfiguration("/**", corsConfiguration);
-                    cors.configurationSource(source);
-                })
                 .logout(logout -> logout
                         .logoutUrl("/api/v1/auth/logout")
 
